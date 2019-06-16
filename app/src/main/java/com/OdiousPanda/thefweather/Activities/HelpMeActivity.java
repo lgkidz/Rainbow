d7817a14cef3183a8ad94c1cee64356685980150
lgkidz <lgkidz1@gmail.com>
2019-06-16 21:14:41 +0700

    prepare for help me activity

--- app/src/main/java/com/OdiousPanda/thefweather/Activities/HelpMeActivity.java
+++ app/src/main/java/com/OdiousPanda/thefweather/Activities/HelpMeActivity.java
@@ -3,6 +3,8 @@ package com.OdiousPanda.thefweather.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.OdiousPanda.thefweather.R;
 
@@ -12,5 +14,18 @@ public class HelpMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_me);
+
+        ImageView btnGoBack = findViewById(R.id.btn_go_back);
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
